463574a7e0e44ec5b4a7b1aac5cce7b82e8a6ba6
chrnavarrete <chnavarrete@hotmail.com>
2025-08-21 20:41:08 -0500

    CreacionProyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+      <h1> Curso de Contenedores!</h1>
+      <p>Hola mundo - EP Newman</p>
+      <h1 style="font-weight: bold;">Christian Navarrete</h1>
+      <p style="color: green; font-size: 18px;">Push Desarrollador 1</p>
+    </div>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
